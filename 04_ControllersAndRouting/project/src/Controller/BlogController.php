5941633358af277c7982fee447777585c89f37a1
Cesar Lopez <clopezlerma@wayfair.com>
2021-01-01 21:26:37 +0100

    Routing - default parameter values

--- 04_ControllersAndRouting/project/src/Controller/BlogController.php
+++ 04_ControllersAndRouting/project/src/Controller/BlogController.php
@@ -30,28 +30,42 @@ class BlogController extends AbstractController {
     ];
 
     /**
-    * @Route("/", name="blog_list")
-    */ 
-    public function list() {
-        return new JsonResponse(self::POSTS);    
+     * @Route("/{page}", name="blog_list", defaults={"page": 5})
+     * @param int $page
+     *
+     * @return JsonResponse
+     */
+    public function list(int $page = 1): JsonResponse {
+        return new JsonResponse(
+            [
+                'page' => $page,
+                'data' => self::POSTS
+            ]
+        );
     }
-    
+
     /**
      * @Route("/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     * @param int $id
+     *
+     * @return JsonResponse
      */
-    public function post(int $id) {
+    public function post(int $id): JsonResponse {
         return new JsonResponse($this->searchByIdAndColumn($id, 'id'));
     }
 
     /**
      * @Route("/{slug}", name="blog_by_slug")
+     * @param $slug
+     *
+     * @return JsonResponse
      */
-    public function postBySlug($slug) {
+    public function postBySlug($slug): JsonResponse {
        return new JsonResponse($this->searchByIdAndColumn($slug, 'slug')); 
     }
 
     private function searchByIdAndColumn(string $id, string $col): ?array {
-        $idx = array_search($id, array_column(self::POSTS, $col));
+        $idx = array_search($id, array_column(self::POSTS, $col), true);
         //return [$idx];
         if ($idx !== false) {
             return self::POSTS[$idx];
